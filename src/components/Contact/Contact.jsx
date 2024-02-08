d962ac000b010d7f46fe6daacf7face312fb55be
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-08 20:26:30 +0700

    added text n delete lorem

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,23 +3,26 @@ import React from "react";
 const Contact = () => {
   return (
     <>
-      <span id="contact"></span>
-      <div data-aos="zoom-in" className="dark:bg-black dark:text-white py-14">
-        <div className="container ">
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
-            <div className="col-span-2 space-y-3">
-              <h1 className="text-4xl sm:text-5xl font-bold text-white">
+      <span id='contact'></span>
+      <div data-aos='zoom-in' className='dark:bg-black dark:text-white py-14'>
+        <div className='container '>
+          <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6'>
+            <div className='col-span-2 space-y-3'>
+              <h1 className='text-4xl sm:text-5xl font-bold text-white'>
                 Let's collaborate on your upcoming car rental venture
               </h1>
-              <p className="text-gray-400">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Exercitationem necessitatibus quasi et vel,{" "}
+              <p className='text-gray-400'>
+                Planning a road trip can be exhilarating, filled with
+                anticipation and the promise of adventure around every bend.
+                From the wind whispering through open windows to the endless
+                possibilities of the open road, every journey becomes a story
+                waiting to unfold.
               </p>
             </div>
-            <div className="sm:grid sm:place-items-center">
+            <div className='sm:grid sm:place-items-center'>
               <a
-                href="#"
-                className="inline-block font-semibold py-2 px-6 bg-primary text-white hover:bg-primary/80 duration-200 tracking-widest uppercase "
+                href='#'
+                className='inline-block font-semibold py-2 px-6 bg-primary text-white hover:bg-primary/80 duration-200 tracking-widest uppercase '
               >
                 Contact
               </a>
