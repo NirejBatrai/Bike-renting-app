0048f2906174254831398978e7c97d0626a72d50
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-08 20:25:09 +0700

    changed the background image

--- src/components/AppStoreBanner/AppStoreBanner.jsx
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import AppStoreImg from "../../assets/website/app_store.png";
 import PlayStoreImg from "../../assets/website/play_store.png";
-import pattern from "../../assets/website/pattern.jpeg";
+import background from "../../assets/background.png";
 
 const bannerImg = {
-  backgroundImage: `url(${pattern})`,
+  backgroundImage: `url(${background})`,
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
@@ -13,39 +13,39 @@ const bannerImg = {
 };
 const AppStoreBanner = () => {
   return (
-    <div className="container">
+    <div className='container'>
       <div
-        className=" text-black py-10 sm:min-h-[400px] sm:grid sm:place-items-center rounded-xl"
+        className=' text-black py-10 sm:min-h-[400px] sm:grid sm:place-items-center rounded-xl'
         style={bannerImg}
       >
         <div>
-          <div className="space-y-6 max-w-xl mx-auto">
+          <div className='space-y-6 max-w-xl mx-auto'>
             <h1
-              data-aos="fade-up"
-              className="text-2xl text-center sm:text-4xl font-semibold font-serif"
+              data-aos='fade-up'
+              className='text-2xl text-center sm:text-4xl font-semibold font-serif'
             >
               Get Started with our app
             </h1>
-            <p data-aos="fade-up" className="text-center sm:px-20">
+            <p data-aos='fade-up' className='text-center sm:px-20'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
               magnam harum accusantium odit?
             </p>
             <div
-              data-aos="fade-up"
-              className="flex flex-wrap justify-center items-center gap-4"
+              data-aos='fade-up'
+              className='flex flex-wrap justify-center items-center gap-4'
             >
-              <a href="#">
+              <a href='#'>
                 <img
                   src={PlayStoreImg}
-                  alt=""
-                  className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                  alt=''
+                  className='max-w-[150px] sm:max-w-[120px] md:max-w-[200px]'
                 />
               </a>
-              <a href="#">
+              <a href='#'>
                 <img
                   src={AppStoreImg}
-                  alt=""
-                  className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                  alt=''
+                  className='max-w-[150px] sm:max-w-[120px] md:max-w-[200px]'
                 />
               </a>
             </div>
