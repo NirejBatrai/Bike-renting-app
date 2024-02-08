8810359dd77b48901972287cc06d3bf127f6bb7e
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-08 20:27:44 +0700

    changed review names

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,63 +1,64 @@
 import React from "react";
 
+import khem from "../../assets/khem.jpg";
+
 const testimonialData = [
   {
-    name: "Dilshad",
-    image: "",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    name: "Khem Khanal",
+    image: { khem },
+    description:
+      "Effortless bike rental process, top-notch bikes, and friendly service made our exploration a joy.",
     aosDelay: "0",
   },
   {
-    name: "Satya",
+    name: "Yasu",
     image: "",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    description:
+      "From start to finish, renting bikes here was a breeze, enhancing our trip with smooth rides and scenic views.",
     aosDelay: "300",
   },
   {
-    name: "Sabir",
+    name: "Nirmal",
     image: "",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    description:
+      "Outstanding bike rental experience, seamless service, and quality bikes made our adventure unforgettable.",
     aosDelay: "1000",
   },
 ];
 const Testimonial = () => {
   return (
     <>
-      <span id="about"></span>
-      <div className="dark:bg-black dark:text-white py-14 sm:pb-24">
-        <div className="container">
+      <span id='about'></span>
+      <div className='dark:bg-black dark:text-white py-14 sm:pb-24'>
+        <div className='container'>
           {/* Header */}
-          <div className="space-y-4 pb-12">
+          <div className='space-y-4 pb-12'>
             <p
-              data-aos="fade-up"
-              className="text-3xl font-semibold text-center sm:text-4xl font-serif"
+              data-aos='fade-up'
+              className='text-3xl font-semibold text-center sm:text-4xl font-serif'
             >
               What Our Clients Say About Us
             </p>
-            <p data-aos="fade-up" className="text-center sm:px-44">
+            <p data-aos='fade-up' className='text-center sm:px-44'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Perferendis iure consectetur tempora amet.
             </p>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-black dark:text-white">
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-black dark:text-white'>
             {testimonialData.map((skill) => (
               <div
                 key={skill.name}
-                data-aos="fade-up"
+                data-aos='fade-up'
                 data-aos-delay={skill.aosDelay}
-                className="card text-center group space-y-3 sm:space-y-6 p-4 sm:py-12 dark:bg-white/20 bg-gray-100 duration-300  rounded-lg "
+                className='card text-center group space-y-3 sm:space-y-6 p-4 sm:py-12 dark:bg-white/20 bg-gray-100 duration-300  rounded-lg '
               >
-                <div className="grid place-items-center ">
-                  <img
-                    src="https://picsum.photos/200"
-                    alt=""
-                    className="rounded-full w-20 h-20"
-                  />
+                <div className='grid place-items-center '>
+                  <img src={khem} alt='' className='rounded-full w-20 h-20' />
                 </div>
-                <div className="text-2xl">⭐⭐⭐⭐⭐</div>
+                <div className='text-2xl'>⭐⭐⭐⭐⭐</div>
                 <p>{skill.description}</p>
-                <p className="text-center font-semibold">{skill.name}</p>
+                <p className='text-center font-semibold'>{skill.name}</p>
               </div>
             ))}
           </div>
