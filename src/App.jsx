c3573be06ac1024ccf2d1d0cd602f8f5a81993c7
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-09 13:54:41 +0700

    install react dom

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
 import CarList from "./components/CarList/CarList";
-import AppStoreBanner from "./components/AppStoreBanner/AppStoreBanner";
+
 import Contact from "./components/Contact/Contact";
 import Testimonial from "./components/Testimonial/Testimonial";
 import Footer from "./components/Footer/Footer";
@@ -50,7 +50,7 @@ const App = () => {
       <Services />
       <CarList />
       <Testimonial />
-      <AppStoreBanner />
+
       <Contact />
       <Footer />
       {/* <Route>
