052e6b033d48db0e4679beb2506e9181cf1da0a2
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-10 16:56:47 +0700

    added BikeDara add images bikeName Description

---
+++ src/components/BikeData/BikeData.js
@@ -0,0 +1,106 @@
+import PCX from "../../assets/PCX.png";
+import Tmax from "../../assets/Tmax.png";
+import Tmaxx from "../../assets/Tmaxx.png";
+
+import XADV from "../../assets/XADV.png";
+import Xmaxx from "../../assets/Xmaxx.png";
+import DirtBikeKawasaki from "../../assets/Dirt-Bike-Kawasaki.png";
+import Harley from "../../assets/Harley-Davidson.png";
+import HondaWave from "../../assets/Honda-wave.png";
+import DirtKtm from "../../assets/KTM-450-DirtBike.png";
+import PCXRed from "../../assets/PCX-honda.png";
+import PCXWhite from "../../assets/PCX-White.png";
+import ScooterHondaWhite from "../../assets/scooterHonda-125cc.png";
+import ScooterHondaBlack from "../../assets/scooterHonda.png";
+import ScooterYamaha from "../../assets/scooterYamaha-155.png";
+import Nmax from "../../assets/Yamaha-Nmax.png";
+import NVX from "../../assets/Yamaha-nvx-155cc.png";
+
+const BikeData = [
+  {
+    imageSrc: PCX,
+    bikeName: "PCX",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: Tmax,
+    bikeName: "Tmax",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: Tmaxx,
+    bikeName: "Tmaxx",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: Xmaxx,
+    bikeName: "Xadv",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: XADV,
+    bikeName: "XADV",
+    description:
+      "Embark on a journey of boundless exploration with the Xadv bike...",
+  },
+  {
+    imageSrc: DirtBikeKawasaki,
+    bikeName: "Dirt Bike Kawasaki",
+    description:
+      "Embark on a journey of boundless exploration with the Xadv bike...",
+  },
+  {
+    imageSrc: Harley,
+    bikeName: "Harley-Davidson",
+    description:
+      "Embark on a journey of boundless exploration with the Xadv bike...",
+  },
+  {
+    imageSrc: HondaWave,
+    bikeName: "Honda Wave",
+    description:
+      "Embark on a journey of boundless exploration with the Xadv bike...",
+  },
+  {
+    imageSrc: DirtKtm,
+    bikeName: "Dirt bike ktm",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: PCXRed,
+    bikeName: "PCX red",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: PCXWhite,
+    bikeName: "PCX white",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: ScooterHondaWhite,
+    bikeName: "ScooterHonda",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: ScooterHondaBlack,
+    bikeName: "ScooterHonda black",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: ScooterYamaha,
+    bikeName: "scooterYamaha-155",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: Nmax,
+    bikeName: "Nmax Yamaha",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+  {
+    imageSrc: NVX,
+    bikeName: "Yamaha nvx 155cc",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  },
+];
+
+export default BikeData;
