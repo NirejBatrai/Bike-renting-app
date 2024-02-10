5c097a8a5ca4902c66cf58ffbe8bf9712284e8d1
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-10 17:02:59 +0700

    add link to Bike rental button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
 import { BiSolidSun, BiSolidMoon } from "react-icons/bi";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import ResponsiveMenu from "./ResponsiveMenu";
 
 export const Navlinks = [
   {
     id: 1,
     name: "HOME",
-    link: "/#",
+    link: "/",
   },
   {
     id: 2,
     name: "BIKE",
-    link: "/#bikes",
+    link: "/pricing", // Update link to "/pricing"
   },
   {
     id: 3,
     name: "ABOUT",
-    link: "/#about",
+    link: "/about",
   },
   {
     id: 4,
     name: "BOOKING",
-    link: "/#booking",
+    link: "/booking",
   },
 ];
 const Navbar = ({ theme, setTheme }) => {
@@ -32,25 +33,24 @@ const Navbar = ({ theme, setTheme }) => {
     setShowMenu(!showMenu);
   };
   return (
-    <div
-      className='relative z-10 shadow-md w-full dark:bg-black dark:text-white duration-300
-    '
-    >
+    <div className='relative z-10 shadow-md w-full dark:bg-black dark:text-white duration-300'>
       <div className='container py-2 md:py-0'>
         <div className='flex justify-between items-center'>
-          <div>
+          <Link
+            to={"/"} // Use Link component with 'to' attribute
+          >
             <span className='text-3xl font-bold font-serif'>Bike Rental</span>
-          </div>
+          </Link>
           <nav className='hidden md:block'>
             <ul className='flex items-center gap-8'>
               {Navlinks.map(({ id, name, link }) => (
                 <li key={id} className='py-4'>
-                  <a
-                    href={link}
-                    className=' text-lg font-medium  hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-colors duration-500  '
+                  <Link
+                    to={link} // Use Link component with 'to' attribute
+                    className=' text-lg font-medium  hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-colors duration-500'
                   >
                     {name}
-                  </a>
+                  </Link>
                 </li>
               ))}
               {/* DarkMode feature implement */}
