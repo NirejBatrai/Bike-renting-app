e57734a986fb987705ecc067cad91a216ce14552
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-10 17:37:58 +0700

    added new submit  button

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -155,11 +155,8 @@ const Booking = () => {
             />
           </div>
           <div>
-            <button
-              type='submit'
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-            >
-              Submit
+            <button type='submit' className='button'>
+              <span> Submit</span>
             </button>
           </div>
         </form>
