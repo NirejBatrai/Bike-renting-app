08cde55891e54a327fd36115452ed447f1ea39f0
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-10 16:49:53 +0700

    fixed the error of routing and move to hero file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React, { Component, useEffect, useState } from "react";
-import { Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 // Component import
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import About from "./components/About/About";
-import Services from "./components/Services/Services";
-import CarList from "./components/CarList/CarList";
-
-import Contact from "./components/Contact/Contact";
-import Testimonial from "./components/Testimonial/Testimonial";
+// import About from "./components/About/About";
+// import Services from "./components/Services/Services";
+// import CarList from "./components/CarList/CarList";
+// import Contact from "./components/Contact/Contact";
+// import Testimonial from "./components/Testimonial/Testimonial";
 import Footer from "./components/Footer/Footer";
-// import Pricing from "./components/BIkes/Pricing";
+import Pricing from "./components/Pricing/Pricing";
+import Booking from "./components/Booking/Booking";
+import BikeDetails from "./components/Details/BikeDetails";
+import About from "./components/About/About";
 
 const App = () => {
   // dark mode start
@@ -42,21 +44,27 @@ const App = () => {
     });
     AOS.refresh();
   }, []);
+
   return (
     <div className='bg-white dark:bg-black dark:text-white text-black overflow-x-hidden'>
-      <Navbar theme={theme} setTheme={setTheme} />
-      <Hero theme={theme} />
-      <About />
-      <Services />
-      <CarList />
-      <Testimonial />
+      <Router>
+        <Navbar theme={theme} setTheme={setTheme} />
 
-      <Contact />
-      <Footer />
-      {/* <Route>
-    
-        <Route path='/pricing' element={<Pricing />} />
-      </Route> */}
+        <Routes>
+          <Route path='/' element={<Hero theme={theme} />} />
+          {/* <About />
+        <Services />
+        <CarList />
+        <Testimonial />
+        <Contact />
+        <Footer /> */}
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/booking' element={<Booking />} />
+          <Route path='/bike-details' element={<BikeDetails />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 };
