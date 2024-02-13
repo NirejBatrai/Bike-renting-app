90c53c4a4fbb7f061632eccc882bf89b08cb89f8
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-13 13:52:43 +0700

    fixed the error..and added taikwin css and add to booking fixed

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,132 +1,164 @@
-import Card from "../../components/Card";
-// import Image1 from "../../assets/Tmax.png";
-// import Image3 from "../../assets/PCX.png";
-// import Image4 from "../../assets/Xmaxx.png";
-// import Image5 from "../../assets/Tmaxx.png";
-// import Image2 from "../../assets/Xadv-750.png";
-import "./Pricing.css";
 import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
-import BikeData from "../BikeData/BikeData";
+import BikeData from "../BikeData/BikeData"; // Importing bike data from BikeData.js
+import { Link } from "react-router-dom";
 
 export default function Pricing() {
-  const titleSpring = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    delay: 600,
-  });
-
-  const cardContainerSpring = useSpring({
-    from: { opacity: 0, transform: "translateX(160px)" },
-    to: { opacity: 1, transform: "translateX(10)" },
-    config: { duration: 500 },
-  });
-
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredCards, setFilteredCards] = useState([]);
+  const [filteredBikes, setFilteredBikes] = useState([]);
 
-  //Function to handle search input change
+  // Function to handle search input change
   const handleSearchInputChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
-    filterCards(query);
+    filterBikes(query);
   };
 
-  //Funtion to filter cards based on the search query
-
-  const filterCards = (query) => {
-    const filtered = BikeData.filter((card) =>
-      card.bikeName.toLowerCase().includes(query.toLowerCase())
+  // Function to filter bikes based on the search query
+  const filterBikes = (query) => {
+    const filtered = BikeData.filter((bike) =>
+      bike.bikeName.toLowerCase().includes(query.toLowerCase())
     );
-    setFilteredCards(filtered);
+    setFilteredBikes(filtered);
   };
+
+  const sortedBike = [...BikeData].sort((a, b) => a.cc - b.cc);
+
   return (
-    <div className='pricing-container'>
-      <div className='input-container'>
+    <div className='container mx-auto px-4 py-8'>
+      <div className='flex items-center justify-between mb-8'>
         <input
           type='text'
           name='text'
-          className='input'
-          placeholder='Search for the bike......'
+          className='border border-gray-300 py-2 px-4 rounded-md w-1/2'
+          placeholder='Search for the bike...'
           value={searchQuery}
           onChange={handleSearchInputChange}
         />
-        <span className='icon'>
-          <svg
-            width='19px'
-            height='19px'
-            viewBox='0 0 24 24'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
-            <g
-              id='SVGRepo_tracerCarrier'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            ></g>
-            <g id='SVGRepo_iconCarrier'>
-              <path
-                opacity='1'
-                d='M14 5H20'
-                stroke='#000'
-                strokeWidth='1.5'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              ></path>
-              <path
-                opacity='1'
-                d='M14 8H17'
-                stroke='#000'
-                strokeWidth='1.5'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              ></path>
-              <path
-                d='M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2'
-                stroke='#000'
-                strokeWidth='2.5'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              ></path>
-              <path
-                opacity='1'
-                d='M22 22L20 20'
-                stroke='#000'
-                strokeWidth='3.5'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              ></path>
-            </g>
-          </svg>
-        </span>
       </div>
 
-      <animated.h1 className='pricing-title' style={titleSpring}>
-        Discover Our Exclusive Models
-      </animated.h1>
+      <h1 className='text-3xl font-bold mb-4'>Discover Our Exclusive Models</h1>
 
-      <animated.div className='card-container' style={cardContainerSpring}>
-        {/* Render filtered cards if search query is present, else render all cards */}
-
+      <div className='grid grid-cols-3 gap-8'>
         {searchQuery !== ""
-          ? filteredCards.map((card, index) => (
-              <Card
+          ? filteredBikes.map((bike, index) => (
+              <div
                 key={index}
-                imageSrc={card.imageSrc}
-                bikeName={card.bikeName}
-                description={card.description}
-              />
+                className='border border-gray-300 p-4 rounded-md flex flex-col items-center'
+              >
+                {/* Bike Image */}
+                <div className='w-32 h-32 mb-4 overflow-hidden'>
+                  <img
+                    src={bike.imageSrc}
+                    alt={bike.bikeName}
+                    className={
+                      bike.bikeName === "Tmax" ||
+                      bike.bikeName === "Nmax Yamaha" ||
+                      bike.bikeName === "Harley-Davidson"
+                        ? "w-full h-auto max-h-24"
+                        : "w-full h-full object-cover"
+                    }
+                  />
+                </div>
+                {/* Bike Name */}
+                <h2 className='text-xl font-bold text-center mb-2'>
+                  {bike.bikeName}
+                </h2>
+                {/* Bike Price */}
+                <p className='text-lg font-semibold'>
+                  Price: ฿{bike.price}/Day
+                </p>
+                {/* Engine Displacement */}
+                <p className='text-sm'>Engine Displacement: {bike.cc} cc</p>
+                {/* Link to Bike Details */}
+                <Link
+                  to={{
+                    pathname: "/bike-details",
+                    search: `?imageSrc=${encodeURIComponent(
+                      bike.imageSrc
+                    )}&bikeName=${encodeURIComponent(
+                      bike.bikeName
+                    )}&description=${encodeURIComponent(bike.description)}`,
+                  }}
+                  className='text-blue-500 hover:underline mb-2'
+                >
+                  Details
+                </Link>
+                {/* Add to Booking Button */}
+                <Link
+                  to={{
+                    pathname: "/booking",
+                    search: `?imageSrc=${encodeURIComponent(
+                      bike.imageSrc
+                    )}&bikeName=${encodeURIComponent(
+                      bike.bikeName
+                    )}&description=${encodeURIComponent(bike.description)}`,
+                  }}
+                  className='text-blue-500 hover:underline mb-2 rounded-lg px-4 py-2  hover:bg-blue-100'
+                >
+                  <button>Add to booking</button>
+                </Link>
+              </div>
             ))
-          : BikeData.map((card, index) => (
-              <Card
+          : sortedBike.map((bike, index) => (
+              <div
                 key={index}
-                imageSrc={card.imageSrc}
-                bikeName={card.bikeName}
-                description={card.description}
-              />
+                className='border border-gray-300 p-4 rounded-md flex flex-col items-center'
+              >
+                {/* Bike Image */}
+                <div className='w-32 h-32 mb-4 overflow-hidden'>
+                  <img
+                    src={bike.imageSrc}
+                    alt={bike.bikeName}
+                    className={
+                      bike.bikeName === "Tmax" ||
+                      bike.bikeName === "Nmax Yamaha" ||
+                      bike.bikeName === "Harley-Davidson"
+                        ? "w-full h-auto max-h-24"
+                        : "w-full h-full object-cover"
+                    }
+                  />
+                </div>
+                {/* Bike Name */}
+                <h2 className='text-xl font-bold text-center mb-2'>
+                  {bike.bikeName}
+                </h2>
+                {/* Bike Price */}
+                <p className='text-lg font-semibold'>
+                  Price: ฿{bike.price}/Day
+                </p>
+                {/* Engine Displacement */}
+                <p className='text-sm'>Engine Displacement: {bike.cc} cc</p>
+                {/* Link to Bike Details */}
+                <Link
+                  to={{
+                    pathname: "/bike-details",
+                    search: `?imageSrc=${encodeURIComponent(
+                      bike.imageSrc
+                    )}&bikeName=${encodeURIComponent(
+                      bike.bikeName
+                    )}&description=${encodeURIComponent(bike.description)}`,
+                  }}
+                  className='text-blue-500 hover:underline mb-2'
+                >
+                  Details
+                </Link>
+                {/* Add to Booking Button */}
+                <Link
+                  to={{
+                    pathname: "/booking",
+                    search: `?imageSrc=${encodeURIComponent(
+                      bike.imageSrc
+                    )}&bikeName=${encodeURIComponent(
+                      bike.bikeName
+                    )}&description=${encodeURIComponent(bike.description)}`,
+                  }}
+                  className='text-blue-500 hover:underline mb-2 rounded-lg px-4 py-2  hover:bg-blue-100'
+                >
+                  <button>Add to booking</button>
+                </Link>
+              </div>
             ))}
-      </animated.div>
+      </div>
     </div>
   );
 }
