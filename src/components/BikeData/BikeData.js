d6d419ffa4bd85030617a76ac20e5b1a7e802391
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-13 13:53:01 +0700

    add cc and price in bike data

--- src/components/BikeData/BikeData.js
+++ src/components/BikeData/BikeData.js
@@ -1,7 +1,5 @@
 import PCX from "../../assets/PCX.png";
 import Tmax from "../../assets/Tmax.png";
-import Tmaxx from "../../assets/Tmaxx.png";
-
 import XADV from "../../assets/XADV.png";
 import Xmaxx from "../../assets/Xmaxx.png";
 import DirtBikeKawasaki from "../../assets/Dirt-Bike-Kawasaki.png";
@@ -10,96 +8,97 @@ import HondaWave from "../../assets/Honda-wave.png";
 import DirtKtm from "../../assets/KTM-450-DirtBike.png";
 import PCXRed from "../../assets/PCX-honda.png";
 import PCXWhite from "../../assets/PCX-White.png";
-import ScooterHondaWhite from "../../assets/scooterHonda-125cc.png";
-import ScooterHondaBlack from "../../assets/scooterHonda.png";
-import ScooterYamaha from "../../assets/scooterYamaha-155.png";
 import Nmax from "../../assets/Yamaha-Nmax.png";
-import NVX from "../../assets/Yamaha-nvx-155cc.png";
 
 const BikeData = [
   {
     imageSrc: PCX,
     bikeName: "PCX",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    description:
+      "The PCX is a sleek and stylish scooter known for its fuel efficiency and smooth ride. It's perfect for urban commuting and short trips around town.",
+    price: 350,
+    cc: 150,
   },
   {
     imageSrc: Tmax,
     bikeName: "Tmax",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  },
-  {
-    imageSrc: Tmaxx,
-    bikeName: "Tmaxx",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    description:
+      "The Tmax is a high-performance maxi-scooter designed for both urban commuting and longer journeys. It offers a perfect blend of power, comfort, and agility.",
+    price: 1400,
+    cc: 530,
   },
   {
     imageSrc: Xmaxx,
-    bikeName: "Xadv",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    bikeName: "Xmax",
+    description:
+      "The Xmax is a high-performance maxi-scooter designed for both urban commuting and longer journeys. It offers a perfect blend of power, comfort, and agility.",
+    price: 700,
+    cc: 250,
   },
+
   {
     imageSrc: XADV,
     bikeName: "XADV",
     description:
-      "Embark on a journey of boundless exploration with the Xadv bike...",
+      "The XADV is a versatile adventure bike that combines the performance of a motorcycle with the practicality of a scooter. It's perfect for riders who want to explore new horizons.",
+    price: 1300,
+    cc: 750,
   },
   {
     imageSrc: DirtBikeKawasaki,
     bikeName: "Dirt Bike Kawasaki",
     description:
-      "Embark on a journey of boundless exploration with the Xadv bike...",
+      "The Kawasaki dirt bike is a true off-road machine built for adrenaline-fueled adventures. With its rugged design and powerful engine, it's ready to conquer any trail.",
+    price: 900,
+    cc: 450,
   },
   {
     imageSrc: Harley,
     bikeName: "Harley-Davidson",
     description:
-      "Embark on a journey of boundless exploration with the Xadv bike...",
+      "The Harley-Davidson is an iconic American motorcycle known for its timeless design and legendary performance. It's a symbol of freedom and adventure on the open road.",
+    price: 1400,
+    cc: 1140,
   },
   {
     imageSrc: HondaWave,
     bikeName: "Honda Wave",
     description:
-      "Embark on a journey of boundless exploration with the Xadv bike...",
+      "The Honda Wave is a popular underbone motorcycle known for its reliability and fuel efficiency. It's the perfect choice for daily commuting and short trips.",
+    price: 200,
+    cc: 110,
   },
   {
     imageSrc: DirtKtm,
     bikeName: "Dirt bike ktm",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    description:
+      "The KTM 450 dirt bike is a championship-winning machine built for extreme off-road riding. With its lightweight chassis and powerful engine, it's ready to tackle the toughest trails.",
+    price: 800,
+    cc: 450,
   },
   {
     imageSrc: PCXRed,
     bikeName: "PCX red",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    description:
+      "The PCX Red is a variant of the popular PCX scooter known for its vibrant color and stylish design. It's the perfect choice for riders who want to stand out from the crowd.",
+    price: 350,
+    cc: 150,
   },
   {
     imageSrc: PCXWhite,
     bikeName: "PCX white",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  },
-  {
-    imageSrc: ScooterHondaWhite,
-    bikeName: "ScooterHonda",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  },
-  {
-    imageSrc: ScooterHondaBlack,
-    bikeName: "ScooterHonda black",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  },
-  {
-    imageSrc: ScooterYamaha,
-    bikeName: "scooterYamaha-155",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    description:
+      "The PCX White is a variant of the PCX scooter featuring a clean and elegant white color scheme. It combines style, comfort, and practicality for urban commuters.",
+    price: 350,
+    cc: 150,
   },
   {
     imageSrc: Nmax,
     bikeName: "Nmax Yamaha",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  },
-  {
-    imageSrc: NVX,
-    bikeName: "Yamaha nvx 155cc",
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    description:
+      "The Nmax Yamaha is a premium scooter known for its sporty design and advanced features. It offers a smooth and comfortable ride, making it perfect for city commuting and weekend getaways.",
+    price: 400,
+    cc: 155,
   },
 ];
 
