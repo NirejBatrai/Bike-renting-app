024080aac4b3c828f4fa71bda5845ea32e6ed1ba
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-18 19:01:28 +0700

    added backend fetching local link to connect database

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
-import BikeData from "../BikeData/BikeData"; // Importing bike data from BikeData.js
+import React, { useEffect, useState } from "react";
+// import BikeData from "../BikeData/BikeData"; // Importing bike data from BikeData.js
 import { Link } from "react-router-dom";
 
 export default function Pricing() {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredBikes, setFilteredBikes] = useState([]);
+
+  const [bikes, setBikes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5003/bikes_details")
+      .then((res) => res.json())
+      .then((data) => setBikes(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   // Function to handle search input change
   const handleSearchInputChange = (event) => {
@@ -15,13 +24,13 @@ export default function Pricing() {
 
   // Function to filter bikes based on the search query
   const filterBikes = (query) => {
-    const filtered = BikeData.filter((bike) =>
+    const filtered = bikes.filter((bike) =>
       bike.bikeName.toLowerCase().includes(query.toLowerCase())
     );
     setFilteredBikes(filtered);
   };
 
-  const sortedBike = [...BikeData].sort((a, b) => a.cc - b.cc);
+  const sortedBike = [...bikes].sort((a, b) => a.cc - b.cc);
 
   return (
     <div className='container mx-auto px-4 py-8'>
