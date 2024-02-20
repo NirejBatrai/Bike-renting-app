419fef95a444dd006bdc3a3119feaccda523d902
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-20 15:02:12 +0700

    Removed the unused Links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -32,105 +32,50 @@ const FooterLinks = [
 
 const Footer = () => {
   return (
-    <div className="bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl">
-      <section className="container">
-        <div className=" grid md:grid-cols-3 py-5">
+    <div className='bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl'>
+      <section className='container'>
+        <div className=' grid md:grid-cols-3 py-5'>
           {/* company Details */}
-          <div className=" py-8 px-4 ">
-            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3 font-serif">
+          <div className=' py-8 px-4 '>
+            <h1 className='sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3 font-serif'>
               Bike Rental
             </h1>
-            <p className="text-sm">
+            <p className='text-sm'>
               Welcome to Bike Rental! We provide premium bikes for your
               adventurous journey. Explore the world on two wheels with us.
             </p>
             <br />
-            <div className="flex items-center gap-3">
+            <div className='flex items-center gap-3'>
               <FaLocationArrow />
               <p>Phuket, Thailand</p>
             </div>
-            <div className="flex items-center gap-3 mt-3">
+            <div className='flex items-center gap-3 mt-3'>
               <FaMobileAlt />
               <p>+66813480300</p>
             </div>
             {/* Social Handle */}
-            <div className="flex items-center gap-3 mt-6">
+            <div className='flex items-center gap-3 mt-6'>
               <a
-               href="https://www.instagram.com/"
-               target="_blank"
-               rel="noopener noreferrer">
-                <FaInstagram className="text-3xl hover:text-primary duration-300" />
-              </a>
-              <a 
-                href="https://www.facebook.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                <FaFacebook className="text-3xl hover:text-primary duration-300" />
-              </a>
-              <a
-                href="https://www.linkedin.com/"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='https://www.instagram.com/'
+                target='_blank'
+                rel='noopener noreferrer'
               >
-                <FaLinkedin className="text-3xl hover:text-primary duration-300" />
+                <FaInstagram className='text-3xl hover:text-primary duration-300' />
               </a>
-            </div>
-          </div>
-          {/* Links */}
-          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10 ">
-            <div className="">
-              <div className="py-8 px-4 ">
-                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                  Important Links
-                </h1>
-                <ul className={`flex flex-col gap-3`}>
-                  {FooterLinks.map((link) => (
-                    <li
-                      key={link.id}
-                      className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200"
-                    >
-                      <span>&#11162;</span>
-                      <span>{link.title}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-
-            <div className="py-8 px-4 ">
-              <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                Links
-              </h1>
-              <ul className={`flex flex-col gap-3`}>
-                {FooterLinks.map((link) => (
-                  <li
-                    key={link.id}
-                    className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200"
-                  >
-                    <span>&#11162;</span>
-                    <span>{link.title}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="py-8 px-4 ">
-              <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                Location
-              </h1>
-              {/* <ul className="list-disc list-inside"> */}
-              <ul className="flex flex-col gap-3">
-                {FooterLinks.map((link) => (
-                  <li
-                    key={link.id}
-                    className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200"
-                  >
-                    <span>&#11162;</span>
-                    <span>{link.title}</span>
-                  </li>
-                ))}
-              </ul>
+              <a
+                href='https://www.facebook.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaFacebook className='text-3xl hover:text-primary duration-300' />
+              </a>
+              <a
+                href='https://www.linkedin.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaLinkedin className='text-3xl hover:text-primary duration-300' />
+              </a>
             </div>
           </div>
         </div>
