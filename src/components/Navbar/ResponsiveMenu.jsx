c06a4907643a7b318d33558527bcc4f3256f915c
Nawarat Chatree <112376464+NawaratKhem@users.noreply.github.com>
2024-02-28 20:59:45 +0700

    Changed the 'a' tag into a 'Link' tag, added 'handleLinkClick' function which sets the showMenu to false when any Link is clicked

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/prop-types */
 import { FaUserCircle } from "react-icons/fa";
-
 import { Navlinks } from "./Navbar";
+import { Link } from "react-router-dom";
 
-const ResponsiveMenu = ({ showMenu }) => {
-  console.log("showMenu", showMenu);
+const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
+  
+    const handleLinkClick = () => {
+      // Toggle the menuOpen state
+       setShowMenu(false);
+    };
+
   return (
     <div
-      className={`${
-        showMenu ? "left-0" : "-left-[100%]"
-      } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
+    className={`${
+      showMenu ? "left-0" : "-left-[100%]"
+    } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
     >
       <div className='card'>
         <div className='flex items-center justify-start gap-3'>
@@ -23,9 +28,9 @@ const ResponsiveMenu = ({ showMenu }) => {
           <ul className='space-y-4 text-xl'>
             {Navlinks.map((data) => (
               <li key={data.id}>
-                <a href={data.link} className='mb-5 inline-block'>
+                <Link to={data.link} onClick={handleLinkClick} className='mb-5 inline-block'>
                   {data.name}
-                </a>
+                </Link>
               </li>
             ))}
           </ul>
